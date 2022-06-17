86844699aff1d4c6447af484990f6d6288e6710f
SourGexko <rldud921209@gmail.com>
2022-06-17 18:32:09 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import io from "socket.io-client";
+import "./App.css";
+
+const socket = io.connect("192.168.10.44:3000");
 
 function App() {
+  const [state, setState] = useState({ message: "", name: "" });
+  const [chat, setChat] = useState([]);
+
+  useEffect(() => {
+    socket.on("events", ({ name, message }) => {
+      setChat([...chat, { name, message }]);
+    });
+  }, [chat]);
+
+  const onTextChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
+
+  const onMessageSubmit = (e) => {
+    e.preventDefault();
+    const { name, message } = state;
+    socket.emit("events", { name, message });
+    setState({ message: "", name });
+  };
+
+  const renderChat = () => {
+    return chat.map(({ name, message }, index) => (
+      <div key={index}>
+        <h3>
+          {name}:<span>{message}</span>
+        </h3>
+      </div>
+    ));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="card">
+      <form onSubmit={onMessageSubmit}>
+        <h1>Message</h1>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <span style={{ width: 30 }}>name</span>
+          <input
+            name="name"
+            onChange={(e) => onTextChange(e)}
+            value={state.name}
+            label="Name"
+          />
+        </div>
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <span style={{ width: 30 }}>message</span>
+          <input
+            name="message"
+            onChange={(e) => onTextChange(e)}
+            value={state.message}
+            id="outlined-multiline-static"
+            variant="outlined"
+            label="Message"
+          />
+        </div>
+        <div
+          style={{ width: "100%", dispaly: "flex", justifyContent: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          <button>Send Message</button>
+        </div>
+      </form>
+      <div className="render-chat">
+        <h1>Chat log</h1>
+        {renderChat()}
+      </div>
     </div>
   );
 }
